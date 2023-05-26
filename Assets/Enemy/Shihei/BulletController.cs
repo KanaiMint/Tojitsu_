9a97de6d000e2002176424475a899961ca500cce
KanaiMint <115054962+KanaiMint@users.noreply.github.com>
2023-05-26 17:32:51 +0900

    adwawda

---
+++ Assets/Enemy/Shihei/BulletController.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletController : MonoBehaviour
+{
+    public EnemyController EnemyController_Script;
+    public Vector3 Vel;
+    public bool IsDead = false;
+    public float lifeTime = 0;
+    public const int KlifeTime = 3;
+    public bool muki = false;
+    public float BulletSpeed=20;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (muki == false)
+        {
+            Vel = new Vector3(-BulletSpeed, 0, 0);
+        }
+        else
+        {
+            Vel = new Vector3(BulletSpeed, 0, 0);
+        }
+        lifeTime = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+            this.transform.position += Vel*Time.deltaTime;
+        lifeTime+= Time.deltaTime;
+        if(lifeTime>2) { Destroy(this.gameObject); }
+       
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+          Destroy(this.gameObject);
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.CompareTag( "Ground"))
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
--- Assets/Enemy/Shihei/EnemyController.cs
+++ Assets/Enemy/Shihei/EnemyController.cs
@@ -1,32 +1,76 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Mathematics;
 using Unity.VisualScripting;
 using UnityEngine;
 
 public class EnemyController : MonoBehaviour
 {
     public PlayerScript Player;
-
-    public Transform Transform;
     public float Movespeed_Second;
-    public Vector2 Vel;
+    public Vector3 Vel;
     public bool Death;
-    public GameObject Bullet;
+    public GameObject BulletPrefab;
     public int HP;
-    
+    public float ShotFlame=0;
+    const int kShotFlame = 0;
+    public bool muki=false;
 
     // Start is called before the first frame update
     void Start()
     {
-        Player = GetComponent<PlayerScript>();
+        //Player = GetComponent<PlayerScript>();
+        ShotFlame = 0;
+        Vel = new Vector2(Movespeed_Second * Time.deltaTime, 0.0f);
     }
 
+    private void FixedUpdate()
+    {
+       
+    }
     // Update is called once per frame
     void Update()
     {
-        if (Vector2.Distance(Player.transform.position, this.transform.position) < 10f) //近くにいたら
-        {
+        transform.position += Vel;
 
+        ShotFlame += Time.deltaTime;
+        Mathf.Clamp(ShotFlame, 0, 50);
+        if (Player != null)
+        {
+            if (Player.transform.position.x < this.transform.position.x)
+            {
+
+                muki = false;
+            }
+            else
+            {
+                muki = true;
+
+            }
+        }
+        if (ShotFlame > 2)
+        {
+                Debug.Log("awdadawdasdwa");
+            //if (Vector2.Distance(Player.transform.position, new Vector2( transform.position.x, transform.position.y)) < 0.5f) //近くにいたら
+            //{
+                GameObject Bullet = Instantiate(BulletPrefab, this.transform.position, Quaternion.identity);
+                BulletController controller = Bullet.GetComponent<BulletController>();
+          
+            controller.muki = muki;
+           
+            
+
+           
+            ShotFlame = kShotFlame;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "")    //カッター
+        {
+            Destroy(this.gameObject);
         }
     }
 }
