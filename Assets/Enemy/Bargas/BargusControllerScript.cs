0d0e7331c9d6b12638b08e124f3a3c106e4369fc
KanaiMint <115054962+KanaiMint@users.noreply.github.com>
2023-05-27 22:58:42 +0900

    bargus

---
+++ Assets/Enemy/Bargas/BargusControllerScript.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BargusControllerScript : MonoBehaviour
+{
+    public Vector3 vel;
+    public float MoveSpeed;
+    public bool isGround;
+    public GroundCheck check;
+    public float isGroundFlame;
+    // Start is called before the first frame update
+    void Start()
+    {
+        check = GetComponent<GroundCheck>();
+        vel = new Vector2(0,-2.0f);
+        isGroundFlame = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += vel*Time.deltaTime;
+        if (check.IsGround())
+        {
+            isGround = true;
+            isGroundFlame += Time.deltaTime;
+            if (isGroundFlame > 2)
+            {
+                vel = new Vector3(0, 4f);
+               
+            }
+            else if (isGroundFlame > 0.25f)
+            {
+                vel = new Vector3(0, -0.7f);
+            }
+        }
+        else
+        {
+            isGround = false;
+            isGroundFlame = 0;
+            vel += new Vector3(0, -0.01f);
+        }
+    }
+
+   
+}
