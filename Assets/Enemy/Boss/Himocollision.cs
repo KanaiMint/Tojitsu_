2560d30375757077edb3162a01df4164418282d8
KanaiMint <k022g0015@g.neec.ac.jp>
2023-05-30 18:42:47 +0900

    awdaw

---
+++ Assets/Enemy/Boss/Himocollision.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Himocollision : MonoBehaviour
+{
+    public bool Hit;
+    public bool Cut;
+    public int HP=3;
+    public GameObject Boss;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if()
+    }
+}
